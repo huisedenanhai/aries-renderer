1277230da25645790c7ff3292dc6e781b3065d59
huisedenanhai <winser@pku.edu.cn>
2022-01-02 13:51:43 +0800

    faster radical inverse

--- src/ars/shaders/include/Sampling.glsl
+++ src/ars/shaders/include/Sampling.glsl
@@ -2,7 +2,7 @@
 #define ARS_SAMPLING_GLSL
 
 // Low-discrepancy sequence for quasi monte carlo intergation
-float halton(float i, float base) {
+float radical_inverse(float i, float base) {
     float v = 0.0;
     float f = 1.0;
     while (i > 0) {
@@ -13,8 +13,17 @@ float halton(float i, float base) {
     return v;
 }
 
-vec2 hammersly(float i, float count) {
-    return vec2(i / count, halton(i, 2.0));
+float radical_inverse_2(uint i) {
+    i = (i << 16u) | (i >> 16u);
+    i = ((i & 0x55555555u) << 1u) | ((i & 0xAAAAAAAAu) >> 1u);
+    i = ((i & 0x33333333u) << 2u) | ((i & 0xCCCCCCCCu) >> 2u);
+    i = ((i & 0x0F0F0F0Fu) << 4u) | ((i & 0xF0F0F0F0u) >> 4u);
+    i = ((i & 0x00FF00FFu) << 8u) | ((i & 0xFF00FF00u) >> 8u);
+    return float(i) * 2.3283064365386963e-10; // / 0x100000000
+}
+
+vec2 hammersly(uint i, uint count) {
+    return vec2(float(i) / float(count), radical_inverse_2(i));
 }
 
 #endif

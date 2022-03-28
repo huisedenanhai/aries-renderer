9c5f0ff4765276ba36ec9e9f05415ae81b96f9a5
huisedenanhai <winser@pku.edu.cn>
2022-03-28 14:15:33 +0800

    move material prop block down

--- src/ars/runtime/render/vk/Material.cpp
+++ src/ars/runtime/render/vk/Material.cpp
@@ -3,8 +3,257 @@
 #include "features/Drawer.h"
 #include <ars/runtime/core/Log.h>
 #include <ars/runtime/core/misc/Visitor.h>
+#include <sstream>
 
 namespace ars::render::vk {
+std::optional<uint32_t> MaterialPropertyBlockInfo::find_property_index(
+    const std::string &prop_name) const {
+    for (int i = 0; i < properties.size(); i++) {
+        if (properties[i].name == prop_name) {
+            return i;
+        }
+    }
+    return std::nullopt;
+}
+
+MaterialPropertyBlockInfo &MaterialPropertyBlockInfo::add_property(
+    const std::string &prop_name,
+    MaterialPropertyType type,
+    const MaterialPropertyVariant &default_value) {
+    MaterialPropertyInfo p{};
+    p.name = prop_name;
+    p.type = type;
+    p.default_value = default_value;
+    properties.push_back(p);
+    return *this;
+}
+
+std::string MaterialPropertyBlockInfo::compile_glsl() const {
+    std::stringstream ss;
+    ss << "struct " << name << " {\n";
+    for (auto &p : properties) {
+        const char *ty_name = "float";
+        switch (p.type) {
+        case MaterialPropertyType::Texture:
+            ty_name = "int";
+            break;
+        case MaterialPropertyType::Float:
+            ty_name = "float";
+            break;
+        case MaterialPropertyType::Float2:
+            ty_name = "vec2";
+            break;
+        case MaterialPropertyType::Float3:
+            ty_name = "vec3";
+            break;
+        case MaterialPropertyType::Float4:
+            ty_name = "vec4";
+            break;
+        }
+        ss << fmt::format("    {} {};\n", ty_name, p.name);
+    }
+
+    ss << "};";
+    return ss.str();
+}
+
+MaterialPropertyBlockLayout::MaterialPropertyBlockLayout(
+    IContext *context, MaterialPropertyBlockInfo info)
+    : _context(context), _info(std::move(info)) {
+    init_data_block_layout();
+}
+
+namespace {
+void property_size_and_alignment(MaterialPropertyType type,
+                                 uint32_t &size,
+                                 uint32_t &alignment) {
+    switch (type) {
+    case MaterialPropertyType::Texture:
+        size = sizeof(uint32_t);
+        alignment = sizeof(uint32_t);
+        break;
+    case MaterialPropertyType::Float:
+        size = sizeof(float);
+        alignment = sizeof(float);
+        break;
+    case MaterialPropertyType::Float2:
+        size = sizeof(float) * 2;
+        alignment = sizeof(float) * 2;
+        break;
+    case MaterialPropertyType::Float3:
+        size = sizeof(float) * 3;
+        alignment = sizeof(float) * 4;
+        break;
+    case MaterialPropertyType::Float4:
+        size = sizeof(float) * 4;
+        alignment = sizeof(float) * 4;
+        break;
+    }
+}
+} // namespace
+
+void MaterialPropertyBlockLayout::init_data_block_layout() {
+    _property_offsets.reserve(_info.properties.size());
+
+    uint32_t cur_ptr = 0;
+    uint32_t max_align = 1;
+
+    auto align_ptr = [](uint32_t ptr, uint32_t align) {
+        assert(align > 0);
+        return ((ptr + align - 1) / align) * align;
+    };
+
+    for (auto &p : _info.properties) {
+        uint32_t prop_size, prop_align;
+        property_size_and_alignment(p.type, prop_size, prop_align);
+        cur_ptr = align_ptr(cur_ptr, prop_align);
+        max_align = std::max(max_align, prop_align);
+        _property_offsets.push_back(cur_ptr);
+        cur_ptr += prop_size;
+    }
+    cur_ptr = align_ptr(cur_ptr, max_align);
+
+    _data_block_size = cur_ptr;
+}
+
+uint32_t MaterialPropertyBlockLayout::property_offset(uint32_t index) const {
+    assert(index < _property_offsets.size());
+    return _property_offsets[index];
+}
+
+uint32_t MaterialPropertyBlockLayout::data_block_size() const {
+    return _data_block_size;
+}
+
+const MaterialPropertyBlockInfo &MaterialPropertyBlockLayout::info() const {
+    return _info;
+}
+
+IContext *MaterialPropertyBlockLayout::context() const {
+    return _context;
+}
+
+MaterialPropertyBlock::MaterialPropertyBlock(
+    std::shared_ptr<MaterialPropertyBlockLayout> layout)
+    : _layout(std::move(layout)) {
+    assert(_layout != nullptr);
+
+    auto &info = _layout->info();
+    _texture_owners.resize(info.properties.size());
+    _data_block.resize(_layout->data_block_size());
+
+    // Init with default value
+    for (int i = 0; i < info.properties.size(); i++) {
+        set_variant_by_index(i, info.properties[i].default_value);
+    }
+}
+
+void MaterialPropertyBlock::set_variant(const std::string &name,
+                                        const MaterialPropertyVariant &value) {
+    auto &info = _layout->info();
+    auto index = info.find_property_index(name);
+    if (!index.has_value()) {
+        ARS_LOG_ERROR(
+            "Material \"{}\" does not have property \"{}\"", info.name, name);
+        return;
+    }
+
+    set_variant_by_index(*index, value);
+}
+
+std::optional<MaterialPropertyVariant>
+MaterialPropertyBlock::get_variant(const std::string &name) {
+    auto &info = _layout->info();
+    auto index = info.find_property_index(name);
+    if (!index.has_value()) {
+        return std::nullopt;
+    }
+    return get_variant_by_index(*index);
+}
+
+std::shared_ptr<MaterialPropertyBlockLayout>
+MaterialPropertyBlock::layout() const {
+    return _layout;
+}
+
+void MaterialPropertyBlock::set_variant_by_index(
+    uint32_t index, const MaterialPropertyVariant &value) {
+    auto &info = _layout->info();
+    assert(index < info.properties.size());
+
+    auto &p = info.properties[index];
+    if (value.type() != p.type) {
+        ARS_LOG_ERROR("Property \"{}\" of material \"{}\" type mismatch",
+                      p.name,
+                      info.name);
+        return;
+    }
+
+    auto data_ptr = &_data_block[_layout->property_offset(index)];
+    std::visit(ars::make_visitor([&](auto &&v) {
+                   using T =
+                       std::remove_cv_t<std::remove_reference_t<decltype(v)>>;
+                   if constexpr (std::is_same_v<T, std::shared_ptr<ITexture>>) {
+                       _texture_owners[index] = v;
+                   } else {
+                       *reinterpret_cast<T *>(data_ptr) = v;
+                   }
+               }),
+               value);
+}
+
+std::vector<std::shared_ptr<ITexture>>
+MaterialPropertyBlock::referenced_textures() {
+    std::vector<std::shared_ptr<ITexture>> textures{};
+    auto props = _layout->info().properties;
+    for (int i = 0; i < props.size(); i++) {
+        if (props[i].type == MaterialPropertyType::Texture) {
+            textures.push_back(get_texture_by_index(i));
+        }
+    }
+    return textures;
+}
+
+MaterialPropertyVariant
+MaterialPropertyBlock::get_variant_by_index(uint32_t index) {
+    auto &info = layout()->info();
+    assert(index < info.properties.size());
+
+    auto &p = info.properties[index];
+    auto data_ptr = &_data_block[_layout->property_offset(index)];
+    switch (p.type) {
+    case MaterialPropertyType::Texture:
+        return get_texture_by_index(index);
+    case MaterialPropertyType::Float:
+        return *reinterpret_cast<float *>(data_ptr);
+    case MaterialPropertyType::Float2:
+        return *reinterpret_cast<glm::vec2 *>(data_ptr);
+    case MaterialPropertyType::Float3:
+        return *reinterpret_cast<glm::vec3 *>(data_ptr);
+    case MaterialPropertyType::Float4:
+        return *reinterpret_cast<glm::vec4 *>(data_ptr);
+    }
+}
+
+std::shared_ptr<ITexture>
+MaterialPropertyBlock::get_texture_by_index(uint32_t index) {
+    auto &info = _layout->info();
+    assert(index < info.properties.size());
+    auto &p = info.properties[index];
+    assert(p.type == MaterialPropertyType::Texture);
+    auto tex = _texture_owners[index];
+    if (tex == nullptr) {
+        if (std::holds_alternative<std::shared_ptr<ITexture>>(
+                p.default_value)) {
+            tex = std::get<std::shared_ptr<ITexture>>(p.default_value);
+        }
+    }
+    if (tex == nullptr) {
+        tex = _layout->context()->default_texture(DefaultTexture::White);
+    }
+    return tex;
+}
+
 MaterialFactory::MaterialFactory(Context *context) : _context(context) {
     init_unlit_template();
     init_metallic_roughness_template();
--- src/ars/runtime/render/vk/Material.h
+++ src/ars/runtime/render/vk/Material.h
@@ -9,6 +9,91 @@ namespace ars::render::vk {
 class Context;
 class GraphicsPipeline;
 
+struct MaterialPropertyBlockInfo {
+    std::string name;
+    std::vector<MaterialPropertyInfo> properties{};
+
+    [[nodiscard]] std::optional<uint32_t>
+    find_property_index(const std::string &prop_name) const;
+
+    MaterialPropertyBlockInfo &
+    add_property(const std::string &prop_name,
+                 MaterialPropertyType type,
+                 const MaterialPropertyVariant &default_value);
+
+    template <typename T>
+    MaterialPropertyBlockInfo &add_property(const std::string &prop_name,
+                                            T &&default_value) {
+        using Type = std::remove_cv_t<std::remove_reference_t<T>>;
+        return add_property(prop_name,
+                            MaterialPropertyTypeTrait<Type>::Type,
+                            std::forward<T>(default_value));
+    }
+
+    std::string compile_glsl() const;
+};
+
+class MaterialPropertyBlockLayout {
+  public:
+    MaterialPropertyBlockLayout(IContext *context,
+                                MaterialPropertyBlockInfo info);
+    const MaterialPropertyBlockInfo &info() const;
+    uint32_t property_offset(uint32_t index) const;
+    uint32_t data_block_size() const;
+    IContext *context() const;
+
+  private:
+    void init_data_block_layout();
+
+    IContext *_context{};
+    MaterialPropertyBlockInfo _info{};
+    std::vector<uint32_t> _property_offsets{};
+    uint32_t _data_block_size{};
+};
+
+class MaterialPropertyBlock {
+  public:
+    explicit MaterialPropertyBlock(
+        std::shared_ptr<MaterialPropertyBlockLayout> layout);
+
+    std::shared_ptr<MaterialPropertyBlockLayout> layout() const;
+
+    // If the property with name is not found, return std::nullopt.
+    // If the property is found and is not set, return default value.
+    // For texture, if the value is nullptr, return default value.
+    // If default texture is nullptr, return default white texture.
+    std::optional<MaterialPropertyVariant> get_variant(const std::string &name);
+
+    // If the property with name is not found, do nothing
+    // If the property is found but type mismatch, do nothing
+    void set_variant(const std::string &name,
+                     const MaterialPropertyVariant &value);
+
+    std::vector<std::shared_ptr<ITexture>> referenced_textures();
+
+    template <typename T> void set(const std::string &name, T &&value) {
+        set_variant(name, std::forward<T>(value));
+    }
+
+    template <typename T> std::optional<T> get(const std::string &name) {
+        auto v = get_variant(name);
+        if (v.has_value() && std::holds_alternative<T>(*v)) {
+            return std::get<T>(*v);
+        }
+        return std::nullopt;
+    }
+
+  private:
+    std::shared_ptr<ITexture> get_texture_by_index(uint32_t index);
+    MaterialPropertyVariant get_variant_by_index(uint32_t index);
+    void set_variant_by_index(uint32_t index,
+                              const MaterialPropertyVariant &value);
+
+    std::shared_ptr<MaterialPropertyBlockLayout> _layout = nullptr;
+    std::vector<uint8_t> _data_block{};
+    std::vector<std::shared_ptr<ITexture>> _texture_owners{};
+};
+
 struct MaterialPass {
     GraphicsPipeline *pipeline = nullptr;
     MaterialPropertyBlock *property_block = nullptr;

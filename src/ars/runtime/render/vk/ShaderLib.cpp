f91c7f7d57ba646dedbdc8276cfca6bf212310b0
huisedenanhai <winser@pku.edu.cn>
2023-07-10 23:18:14 +0800

    use new frill

--- src/ars/runtime/render/vk/ShaderLib.cpp
+++ src/ars/runtime/render/vk/ShaderLib.cpp
@@ -1,15 +1,34 @@
 #include "Vulkan.h"
 #include <ars/runtime/core/Log.h>
-#include <frill_shaders.hpp>
+#include <frill.h>
+#include <set>
+#include <sstream>
 
 namespace ars::render::vk {
 MemoryView
 load_spirv_code(const char *path, const char **flags, uint32_t flag_count) {
-    auto code = frill_shaders::load(path, flags, flag_count);
-    if (code.code == nullptr) {
+    void *code = {};
+    size_t size = {};
+    std::set<std::string> unique_flags{};
+    for (int i = 0; i < flag_count; i++) {
+        unique_flags.emplace(flags[i]);
+    }
+    std::stringstream ss{};
+    bool is_first = true;
+    for (auto &flag : unique_flags) {
+        if (is_first) {
+            is_first = false;
+        } else {
+            ss << ",";
+        }
+        ss << flag;
+    }
+    auto uri = fmt::format("{}/flags={}", path, ss.str());
+    frill::get_asset_bytes(uri.c_str(), &code, &size);
+    if (code == nullptr) {
         ARS_LOG_CRITICAL("Shader {} not found, have you add it to frill.json?",
                          path);
     }
-    return MemoryView{code.code, code.size};
+    return MemoryView{reinterpret_cast<uint8_t *>(code), size};
 }
 } // namespace ars::render::vk
